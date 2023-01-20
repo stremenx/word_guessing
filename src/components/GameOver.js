05b338893aa11f0f438f038a591983d54b1fc02d
Eduardo Soares <stremenx@gmail.com>
2023-01-20 16:46:39 -0300

    Update 0.04
     Criando componentes do projeto.

---
+++ src/components/GameOver.js
@@ -0,0 +1,9 @@
+import './GameOver.css';
+
+const GameOver = () => {
+  return (
+    <div>Gameover</div>
+  )
+}
+
+export default GameOver
--- src/data/words.js
+++ src/data/words.js
@@ -1,8 +1,8 @@
 export const wordsList = {
-    caroo: ["Motor", "Porta", "Pneu" "Antena"],
-    fruta: ["Banana", "Laranja", "Goiaba", "Melancia",],
-    corpo: ["Braço","Perna","Cérebro","Pescoço","Olho",],
-    computador: ["Mouse","Teclado","Monitor","Gabinete",],
-    programação: ["Linguagem", "Framework", "JavaScript", "React",],
-    alimento: ["Arroz","Feijão", "Carne", "Leite", "Ovo",]
+    caroo: ["Motor", "Porta", "Pneu", "Antena"],
+    fruta: ["Banana", "Laranja", "Goiaba", "Melancia"],
+    corpo: ["Braço","Perna","Cérebro","Pescoço","Olho"],
+    computador: ["Mouse","Teclado","Monitor","Gabinete"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React"],
+    alimento: ["Arroz","Feijão", "Carne", "Leite", "Ovo"]
 };
