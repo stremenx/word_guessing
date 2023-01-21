6043a22028e86cac4e35b2aa6b5444739bdefb3c
Eduardo Soares <stremenx@gmail.com>
2023-01-21 09:43:05 -0300

    Upgrade 0.06
     Escolhendo palavra e categoria de forma aleatória.

--- src/App.js
+++ src/App.js
@@ -24,8 +24,40 @@ function App() {
  const [gameStage, setGameStage] = useState(stages[0].name);
  const [words] = useState(wordsList);
 
+ const [pickWord, setPickWord] = useState("");
+ const [pickedCategories, setPickedCategory] = useState("");
+ const [letters, setLetters] = useState([]);
+
+const pickWordAndCategory = () => {
+  //Pick word and pick category
+  const categories = Object.keys(words);
+  const category = categories[Math.floor(Math.random() * Object.keys(categories).length)] 
+
+  //Pick a random word
+  const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+  return {word, category}
+};
+
+
 //start the secret word game
 const startGame = () => {
+
+  //Pick word and pick category
+  const {word, category} = pickWordAndCategory();
+
+  //create an array of letters
+  let wordLetters = word.split("")
+  wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+  console.log(word, category)
+  console.log(wordLetters)
+
+  // fill states
+  setPickWord(word);
+  setPickedCategory(category);
+  setLetters(letters);
+
   setGameStage(stages[1].name);
 };
 
--- src/data/words.js
+++ src/data/words.js
@@ -1,5 +1,5 @@
 export const wordsList = {
-    caroo: ["Motor", "Porta", "Pneu", "Antena"],
+    carro: ["Motor", "Porta", "Pneu", "Antena"],
     fruta: ["Banana", "Laranja", "Goiaba", "Melancia"],
     corpo: ["Braço","Perna","Cérebro","Pescoço","Olho"],
     computador: ["Mouse","Teclado","Monitor","Gabinete"],
